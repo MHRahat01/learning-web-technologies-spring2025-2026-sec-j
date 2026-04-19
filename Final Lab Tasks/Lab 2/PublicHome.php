48848698a63cba662ca0282b2c3b8e9d99388de8
MHRahat01 <rahathasan0123@gmail.com>
2026-04-19 14:05:44 +0600

    Lab 2 A

---
+++ Final Lab Tasks/Lab 2/PublicHome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Public Home</title>
+</head>
+<body>
+
+<h2>Public Home</h2>
+
+<a href="registration.php">Registration</a><br>
+<a href="login.php">Login</a><br>
+<a href="forgot_password.php">Forgot Password</a>
+
+</body>
+</html>
