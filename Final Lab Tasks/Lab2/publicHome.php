1fc9bd73f20dac4e64b42602451b8f8e3184d840
MHRahat01 <rahathasan0123@gmail.com>
2026-04-27 22:03:12 +0600

    Lab 2 added

---
+++ Final Lab Tasks/Lab2/publicHome.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Public Home</title>
+</head>
+<body>
+    <table border="1" width="100%" cellspacing="0" cellpadding="10">
+        <tr>
+            <td>
+                <h1 style="display: inline; color: green;">X</h1>
+                <h2 style="display: inline;">Company</h2>
+            </td>
+            <td align="right">
+                <a href="public_home.php">Home</a> | 
+                <a href="login.php">Login</a> | 
+                <a href="registration.php">Registration</a>
+            </td>
+        </tr>
+
+        <tr height="200px">
+            <td colspan="2" valign="top">
+                <br>
+                <b>Welcome to xCompany</b>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2" align="center">
+                Copyright © 2017
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
